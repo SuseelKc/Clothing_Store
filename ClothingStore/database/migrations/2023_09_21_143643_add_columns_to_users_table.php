02d47551fd9adabd52801ab2ca70800e5a83195e
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-09-21 20:23:07 +0545

    database updated of user table

---
+++ ClothingStore/database/migrations/2023_09_21_143643_add_columns_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('address')->nullable();
+            $table->string('contact')->nullable();
+            $table->string('usertype')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('address');
+            $table->dropColumn('contact');
+            $table->dropColumn('usertype');
+        });
+    }
+};
--- ClothingStore/resources/views/auth/register.blade.php
+++ ClothingStore/resources/views/auth/register.blade.php
@@ -15,6 +15,7 @@
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+        
 
         <!-- Password -->
         <div class="mt-4">
