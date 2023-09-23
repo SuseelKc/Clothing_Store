30942af27f9df33a13592a918171e43c31789379
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-09-23 19:30:08 +0545

    added header for login,register,profile

--- ClothingStore/resources/views/auth/register.blade.php
+++ ClothingStore/resources/views/auth/register.blade.php
@@ -1,4 +1,30 @@
+<meta charset="UTF-8">
+    <meta name="description" content="Ashion Template">
+    <meta name="keywords" content="Ashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ashion | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="home/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="home/css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="home/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/style.css" type="text/css">
+</head>
+
+    @include('home.header')
 <x-guest-layout>
+<head>
+    
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
---
+++ ClothingStore/resources/views/home/css_js.blade.php
@@ -0,0 +1,28 @@
+<!-- Google Font -->
+<link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="home/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="home/css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="home/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="home/css/style.css" type="text/css">
+
+
+
+<!-- Js Plugins -->
+<script src="home/js/jquery-3.3.1.min.js"></script>
+<script src="home/js/bootstrap.min.js"></script>
+<script src="home/js/jquery.magnific-popup.min.js"></script>
+<script src="home/js/jquery-ui.min.js"></script>
+<script src="home/js/mixitup.min.js"></script>
+<script src="home/js/jquery.countdown.min.js"></script>
+<script src="home/js/jquery.slicknav.js"></script>
+<script src="home/js/owl.carousel.min.js"></script>
+<script src="home/js/jquery.nicescroll.min.js"></script>
+<script src="home/js/main.js"></script>
