d66a59da9dd837f046dc3ad1473ebc2d7f196c54
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-09-24 10:36:12 +0545

    navbar updated

--- ClothingStore/resources/views/home/header.blade.php
+++ ClothingStore/resources/views/home/header.blade.php
@@ -10,19 +10,18 @@
                     <nav class="header__menu">
                         <ul>
                             <li class="active"><a href="{{url('/')}}">Home</a></li>
-                            <li><a href="#">Women’s</a></li>
-                            <li><a href="#">Men’s</a></li>
-                            <li><a href="./shop.html">Shop</a></li>
-                            <li><a href="#">Pages</a>
+                            <!-- <li><a href="#">Women’s</a></li>
+                            <li><a href="#">Men’s</a></li> -->
+                            <li><a href="./shop.html">Products</a></li>
+                            <li><a href="#">Category</a>
                                 <ul class="dropdown">
-                                    <li><a href="./product-details.html">Product Details</a></li>
-                                    <li><a href="./shop-cart.html">Shop Cart</a></li>
-                                    <li><a href="./checkout.html">Checkout</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
+                                    <li><a href="./product-details.html">Women’s</a></li>
+                                    <li><a href="./shop-cart.html">Men’s</a></li>
                                 </ul>
                             </li>
-                            <li><a href="./blog.html">Blog</a></li>
                             <li><a href="./contact.html">Contact</a></li>
+                            <li><a href="./blog.html">About Us</a></li>
+                            
                         </ul>
                     </nav>
                 </div>
