0539bc55640aff8401442649f30f670f86651697
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-09-26 20:49:40 +0545

    footer update

--- ClothingStore/resources/views/home/footer.blade.php
+++ ClothingStore/resources/views/home/footer.blade.php
@@ -60,7 +60,7 @@
             <div class="col-lg-12">
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 <div class="footer__copyright__text">
-                    <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                    <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | Developed <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Infinity Innovation</a></p>
                 </div>
                 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </div>
