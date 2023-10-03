a01c47fb202eaf5d8292f0671a5db459be67d0d7
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-10-03 15:25:27 +0545

    purchasecode done

--- ClothingStore/app/Http/Controllers/OrderController.php
+++ ClothingStore/app/Http/Controllers/OrderController.php
@@ -14,14 +14,18 @@ class OrderController extends Controller
 {
     public function cash_order(Request $request)
     {
+        
         $user_id = Auth::user()->id;
         //order_master
         $order_master = new OrderMaster;
         $order_master->user_id = $user_id;
+        //for purchase code
+        $order_master->purchasecode = $this->PurchaseCode(); 
+
         $order_master->payment_type = PaymentType::CashOnDelivery;
         $order_master->totalamount = $request->input('totalAmount');
         $order_master->save();
-        //order
+        //for order data
         $data = Cart::where('user_id','=',$user_id)->get();
         foreach($data as $data)
         {
@@ -33,11 +37,27 @@ class OrderController extends Controller
             $order->rate = $data->rate;
             $order->amount = $data->price;
             $order->save();
+
             //for deleting same data in cart
             $cart_id = $data->id;
             $cart = Cart::find($cart_id);
             $cart->delete();
         }
+        
         return view('home.thankyou');
+    }
+    private function PurchaseCode()
+    {
+        $latestPurchaseMaster = OrderMaster::latest('id')->first();
+        if ($latestPurchaseMaster) {
+            $lastCode = $latestPurchaseMaster->purchasecode;
+            $parts = explode('-', $lastCode);
+            $lastNumber = (int)end($parts);
+            $newNumber = str_pad($lastNumber + 1, 3, '0', STR_PAD_LEFT);
+            $newCode = $parts[0] . '-' . $newNumber;
+        } else {
+            $newCode = 'PU-001'; // Initial code if no previous records exist
+        }
+        return $newCode;
     }
 }
