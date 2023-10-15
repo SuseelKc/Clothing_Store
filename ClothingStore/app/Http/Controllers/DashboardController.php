eadb35764acfcac6528d7a53c93d6587e6f045be
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-10-15 11:57:40 +0545

    total users in admin dashboard

--- ClothingStore/app/Http/Controllers/DashboardController.php
+++ ClothingStore/app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cart;
 use App\Models\Category;
 use App\Models\Products;
+use App\Models\User;
 use App\Models\OrderMaster;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,10 @@ class DashboardController extends Controller
     //
     public function index(){
         $order=OrderMaster::count();
+        $user = User::where('usertype', 0)->count();
         $product=Products::count();
         $category=Category::count();
-        return view('admin.dashboard',compact('order','product','category'));
+        return view('admin.dashboard',compact('order','product','category','user'));
     }
     public function aboutus(){
         $categories = Category::all();
