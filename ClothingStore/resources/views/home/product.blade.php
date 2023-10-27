6699fd48818911132048ba811f25932d3c5f637d
Sushil Khatri <139630872+SuseelKc@users.noreply.github.com>
2023-10-27 20:44:42 +0545

    price display of discount instead of normal price

--- ClothingStore/resources/views/home/product.blade.php
+++ ClothingStore/resources/views/home/product.blade.php
@@ -135,7 +135,12 @@
                             <i class="fa fa-star"></i>
                             <i class="fa fa-star"></i>
                         </div>
-                        <div class="product__price">Rs. {{$products->price}}</div>
+
+                        @if($products->discounted_price)
+                            <div class="product__price">Rs. {{$products->discounted_price}}</div>
+                        @else
+                            <div class="product__price">Rs. {{$products->price}}</div>
+                        @endif
                         
                     </div>
                 </div>
