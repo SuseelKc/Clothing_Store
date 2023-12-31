1338ce31b4d3ac5a7c70d603ae2366b2449ef4aa
SuseelKc <sushil.np.khatri@gmail.com>
2023-12-31 14:02:05 +0545

    login changes

--- ClothingStore/resources/views/auth/login.blade.php
+++ ClothingStore/resources/views/auth/login.blade.php
@@ -48,15 +48,24 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+            <div class="container" style="padding-right: 10px">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
+                <br>
+            {{--  --}}
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                {{ __('Create New Account') }}
+            </a>
+            {{--  --}}
+            </div>
+            <div class="container" style="padding-left: 97px" >
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
             </x-primary-button>
+            </div>
         </div>
     </form>
 </x-guest-layout>
